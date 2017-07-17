37fe02634352547c7d7d176931f0428af7f1457f
humbledude <keunhui.park@gmail.com>
2017-07-17 13:56:08 +0900

    AlarmManager test update

--- app/src/androidTest/java/com/humbledude/alarmtest/AlarmManagerSetTest.java
+++ app/src/androidTest/java/com/humbledude/alarmtest/AlarmManagerSetTest.java
@@ -9,12 +9,16 @@ import android.support.annotation.RequiresApi;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.text.format.DateUtils;
+import android.util.Log;
 
 import dalvik.annotation.TestTarget;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by keunhui.park on 2017. 7. 14..
@@ -36,25 +40,62 @@ public class AlarmManagerSetTest {
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Test
-    public void test() throws InterruptedException {
+    public void test1() throws InterruptedException {
+        Map<String, Object> checkMap = new HashMap<>();
+
         Intent intent = new Intent(context, MainReceiver.class);
 
         intent.putExtra("key_test", "value_test0");
         long triggerTime = System.currentTimeMillis() + 30 * DateUtils.SECOND_IN_MILLIS;
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
         mAlarmManager.set(AlarmManager.RTC_WAKEUP, triggerTime, pi);
+        checkMap.put("value_test0", false);
 
         intent.putExtra("key_test", "value_test1");
         long triggerTime1 = System.currentTimeMillis() + 60 * DateUtils.SECOND_IN_MILLIS;
         PendingIntent pi1 = PendingIntent.getBroadcast(context, 1, intent, 0);
         mAlarmManager.setExact(AlarmManager.RTC_WAKEUP, triggerTime1, pi1);
+        checkMap.put("value_test1", false);
 
         intent.putExtra("key_test", "value_test2");
         long triggerTime2 = System.currentTimeMillis() + 90 * DateUtils.SECOND_IN_MILLIS;
         PendingIntent pi2 = PendingIntent.getBroadcast(context, 2, intent, 0);
         mAlarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, triggerTime2, pi2);
+        checkMap.put("value_test2", false);
 
-        Thread.sleep(3 * DateUtils.MINUTE_IN_MILLIS);
+        boolean clear = false;
+        long count = 0;
+        do {
+            if (MainReceiver.getInstance() != null) {
+                if (MainReceiver.getInstance().valueSet.contains("value_test0")) {
+                    if (!(Boolean)checkMap.get("value_test0")) {
+                        Log.i(TAG, "value_test0 received : " + count/1000L);
+                        checkMap.put("value_test0", true);
+                    }
+                }
+                if (MainReceiver.getInstance().valueSet.contains("value_test1")) {
+                    if (!(Boolean)checkMap.get("value_test1")) {
+                        Log.i(TAG, "value_test1 received : " + count/1000L);
+                        checkMap.put("value_test1", true);
+                    }
+                }
+                if (MainReceiver.getInstance().valueSet.contains("value_test2")) {
+                    if (!(Boolean)checkMap.get("value_test2")) {
+                        Log.i(TAG, "value_test2 received : " + count/1000L);
+                        checkMap.put("value_test2", true);
+                    }
+                }
+
+                clear = (Boolean)checkMap.get("value_test0")
+                    && (Boolean)checkMap.get("value_test1")
+                    && (Boolean)checkMap.get("value_test2");
+            }
+
+            Thread.sleep(1000L);
+            count += 1000L;
+        } while (!clear);
+
+        Log.e(TAG, "it took " + count/1000L + " sec");
 
     }
 
--- app/src/main/java/com/humbledude/alarmtest/MainReceiver.java
+++ app/src/main/java/com/humbledude/alarmtest/MainReceiver.java
@@ -5,6 +5,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by keunhui.park on 2017. 7. 14..
  */
@@ -12,15 +17,34 @@ import android.util.Log;
 public class MainReceiver extends BroadcastReceiver {
     private static final String TAG = MainReceiver.class.getSimpleName();
 
-    public static String valTest = "";
+    private static MainReceiver INSTANCE;
+
+    public Set<String> valueSet = new HashSet<>();
+
+    public MainReceiver() {
+        INSTANCE = this;
+    }
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.e(TAG, "onReceive : " + intent.toString());
 
-        if (intent.hasExtra("key_test")) {
-            valTest = intent.getStringExtra("key_test");
-            Log.e(TAG, "key_test : " + valTest);
+        synchronized (MainReceiver.class) {
+            if (INSTANCE == null) {
+                INSTANCE = this;
+            }
         }
+
+        if (intent.hasExtra("key_test")) {
+            String valTest = intent.getStringExtra("key_test");
+            Log.e(TAG, "key_test : " + valTest);
+            valueSet.add(valTest);
+        }
+    }
+
+
+    public static MainReceiver getInstance() {
+        return INSTANCE;
     }
 }
